7d23032565a2199c9bc5d877095fe80a8110fbb1
Pedro Modesto <pedrogabrielmodesto@gmail.com>
2024-10-05 10:54:54 -0300

    Função de registro: - Atualizada com mais verificações. - Código padronizado em inglês.

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,28 +1,49 @@
 const usersConfig = require('../DB/usersConfig.js') //pegando a lista do DB
 const jwt = require('jsonwebtoken') //Chamando a função JWT
+const shortid = require('shortid')
+
 const secretKey = process.env.secret_key
 
 exports.homepage = (req, res) =>{
-    res.status(200).json({aviso:'funcionou'})
+    res.status(200).json({alert:'funcionou'})
 }
 
 //Função de registrar, começa com export para ser usada fora desse arquivo
 exports.register = (req, res)=>{
-    const {username, password} = req.body
+    const {username, password, email, phone} = req.body
 
-    const users = usersConfig.getUsers()
-    //Tratamento para caso o user exista e não prosseguir cadastando por cima
-    const userExistente = users.find(user => user.username === username)
-    if(userExistente){
-        return res.status(400).json({aviso:'O username já existe no banco de dados'})
+    if(username.length < 6){
+        return res.status(401).json({alert: "Apelido muito pequeno"})
     }
 
+    if(password.length < 8 || !/[A-Z]/.test(password) || !/[0-9]/.test(password) || !/[!@#$%^&*(),.?":{}|<>]/.test(password)){
+        return res.status(401).json({alert:"Senha não atende os requisitos"})
+    }
+
+    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+        return res.status(401).json({alert:"Email inválido!"})
+    }
+
+    if(phone.length < 11){
+        return res.status(401).json({alert:"Número inválido"})
+    }
+
+    const users = usersConfig.getUsers()
+
+    //Tratamento para caso o user exista e não prosseguir cadastando por cima
+    const userExists = users.find(user => user.email === email)
+    if(userExists){
+        return res.status(400).json({alert:"O usuario já existe no banco de dados"})
+    }
+
+    const uniqueId = shortid.generate()
+    
     //Add user no DB
-    const newUser = {username: username, password: password}
+    const newUser = {id: uniqueId, username: username, password: password, email: email, phone: phone}
     usersConfig.addUser(newUser)
 
     //resposta
-    res.status(200).json({aviso: 'Usuario cadastrado com sucesso'})
+    res.status(200).json({alert: "Usuario cadastrado com sucesso"})
 }
 
 //Função de login > verifico se user existe / se senha ta correta / gero o token / envia token como resposta
@@ -31,17 +52,17 @@ exports.login = (req,res) => {
 
     const  users = usersConfig.getUsers()
 
-    const userExistente = users.find(user => user.username === username)
-    if(!userExistente){
-        return res.status(401).json({aviso:'Usuario inexistente no DB'})
+    const userExists = users.find(user => user.username === username)
+    if(!userExists){
+        return res.status(401).json({alert:"Usuario inexistente no DB"})
     }
 
-    const passwordOk = password === userExistente.password
+    const passwordOk = password === userExists.password
     if(!passwordOk){
-        return res.status(400).json({aviso:'Senha incorreta!'})
+        return res.status(400).json({alert:"Senha incorreta!"})
     }
 
-    const token = jwt.sign({username: userExistente.username}, secretKey, {
+    const token = jwt.sign({username: userExists.username}, secretKey, {
         expiresIn: '2 days'
     })
 
@@ -59,24 +80,24 @@ exports.editUser = (req, res) => {
     const {username, password, newPassword} = req.body
 
     if(!newPassword){
-        return res.status(401).json({aviso:"Faltando a nova senha!"})
+        return res.status(401).json({alert:"Faltando a nova senha!"})
     }
 
     const users = usersConfig.getUsers()
 
     const userIndex = users.findIndex(user => user.username === username)
     if(userIndex === -1){
-        return res.status(400).json({aviso:"Usuario não existe no DB!"})
+        return res.status(400).json({alert:"Usuario não existe no DB!"})
     }
 
-    const userExistente = users[userIndex]
+    const userExists = users[userIndex]
 
-    if(userExistente.password === password){
+    if(userExists.password === password){
         users[userIndex].password = newPassword
         usersConfig.editUser(users)
     }else{
-        return res.status(401).json({aviso:"A senha não confere"})
+        return res.status(401).json({alert:"A senha não confere"})
     }
 
-    res.status(200).json({aviso:'Usuario editado com sucesso'})
+    res.status(200).json({alert:"Usuario editado com sucesso"})
 }
